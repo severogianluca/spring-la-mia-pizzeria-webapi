e27bbce33b0f929b23fa19e1dbfc105664ed3997
severogianluca <gianlucasevero300@gmail.com>
2025-08-20 04:43:41 +0200

    Feat: add service

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/service/PizzaService.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/service/PizzaService.java
@@ -1,5 +1,8 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.service;
 
+import java.util.List;
+
+import org.lessons.java.spring_la_mia_pizzeria_crud.model.PizzaModel;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +12,29 @@ public class PizzaService {
 
     @Autowired
     private PizzaRepository pizzaRepository;
-    
+
+    public List<PizzaModel> findAll() {
+        return pizzaRepository.findAll();
+    }
+
+    public PizzaModel findById(Integer id) {
+        return pizzaRepository.findById(id).get();
+    }
+
+    public List<PizzaModel> findByname(String nome) {
+        return pizzaRepository.findByNomeContaining(nome);
+    }
+
+
+    // create e update 
+    public PizzaModel save(PizzaModel pizza) {
+        return pizzaRepository.save(pizza);
+
+    }
+
+    public void deleteById(Integer id) {
+        pizzaRepository.deleteById(id);
+    }
+
+
 }
